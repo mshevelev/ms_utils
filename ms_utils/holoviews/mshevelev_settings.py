3124a46cc07fc68a7774f48bad4717e6524f27e5
Mikhail Shevelev <shevelev.mikhail@gmail.com>
2023-05-01 22:23:20 -0400

    added defaults for hv.Image

--- ms_utils/holoviews/mshevelev_settings.py
+++ ms_utils/holoviews/mshevelev_settings.py
@@ -1,23 +1,48 @@
 import bokeh.models
 import holoviews as hv
 
+
 def apply_defaults():
 
-  hv.opts.defaults(
-    hv.opts.Curve(show_grid=True, padding=0.03,
-                  default_tools=['xbox_zoom', 'box_zoom', 'pan', 'undo', 'redo', 'reset'],
-                  active_tools=['xbox_zoom'],
-                  toolbar='above'
-                  ),
-    hv.opts.Scatter(show_grid=True, padding=0.03,
-                   default_tools=['box_zoom', 'pan', 'undo', 'redo', 'reset'],
-                   toolbar='above'
-                   ),
-    hv.opts.BoxWhisker(show_grid=True, toolbar='above'),
-    hv.opts.Bars(show_grid=True, xrotation=90, padding=0.03, toolbar='above'),
-    hv.opts.Histogram(show_grid=True, toolbar='above'),
-    hv.opts.Distribution(show_grid=True, toolbar='above'),
-    hv.opts.Overlay(click_policy='hide', toolbar='above', legend_position='right'),
-    hv.opts.NdOverlay(click_policy='hide', toolbar='above', legend_position='right'),
-    hv.opts.Layout(merge_tools=False),
-  )
+    hv.opts.defaults(
+        hv.opts.Curve(show_grid=True, padding=0.03,
+                      default_tools=['xbox_zoom', 'box_zoom',
+                                     'pan', 'undo', 'redo', 'reset'],
+                      active_tools=['xbox_zoom'],
+                      toolbar='above'
+                      ),
+        hv.opts.Scatter(show_grid=True, padding=0.03,
+                        default_tools=['box_zoom', 'pan',
+                                       'undo', 'redo', 'reset'],
+                        toolbar='above'
+                        ),
+        hv.opts.BoxWhisker(show_grid=True, toolbar='above'),
+        hv.opts.Bars(show_grid=True, xrotation=90, padding=0.03,
+                     default_tools=['box_zoom', 'pan',
+                                    'wheel_zoom', 'undo', 'redo', 'reset'],
+                     active_tools=['pan'],
+                     toolbar='above'),
+        hv.opts.Histogram(show_grid=True,
+                          default_tools=['box_zoom', 'pan',
+                                         'wheel_zoom', 'undo', 'redo', 'reset'],
+                          active_tools=['pan'],
+                          toolbar='above',
+                          ),
+        hv.opts.Distribution(show_grid=True,
+                             default_tools=['box_zoom', 'pan',
+                                            'wheel_zoom', 'undo', 'redo', 'reset'],
+                             active_tools=['pan'],
+                             toolbar='above'
+                             ),
+        hv.opts.Image(show_grid=True, padding=0.01,
+                      default_tools=[ 'box_zoom', "wheel_zoom",
+                                     'pan', 'undo', 'redo', 'reset'],
+                      active_tools=['box_zoom'],
+                      toolbar='above'
+                      ),
+        hv.opts.Overlay(click_policy='hide', toolbar='above',
+                        legend_position='right'),
+        hv.opts.NdOverlay(click_policy='hide', toolbar='above',
+                          legend_position='right'),
+        hv.opts.Layout(merge_tools=False),
+    )
